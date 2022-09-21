63a91c75cd1db13495a33e00c7e66cd490510124
Toky Chanthavong <tchantha01@gmail.com>
2022-09-21 11:55:38 -0600

    deployment demo

---
+++ index.js
@@ -0,0 +1,27 @@
+//run npm init -y
+//npm i express
+//add boilerplate to server
+const express = require('express')
+
+
+//access to files
+const path = require('path')
+// require('dotenv').config()
+
+//uses Heroku port or defaults to 5000
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+//set up request
+//get endpoint for index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+app.listen(port, () => {
+    console.log('Docked at port ' + port)
+})
+
+//install new package, npm i dotenv
