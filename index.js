07227507e9eced235775ba40ba904fa8b2c4eb33
Toky Chanthavong <tchantha01@gmail.com>
2022-09-21 12:39:55 -0600

    deployment demo

--- index.js
+++ index.js
@@ -10,18 +10,22 @@ require('dotenv').config()
 
 //uses Heroku port or defaults to 5000
 const port = process.env.PORT || 5000
-
 const app = express()
-
 app.use(express.json())
+
+
 
 //set up request
 //get endpoint for index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'))
+  })
+  
 app.listen(port, () => {
     console.log('Docked at port ' + port)
 })
 
-//install new package, npm i dotenv
